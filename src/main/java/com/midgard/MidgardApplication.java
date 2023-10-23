a39c5dca60e62dc9c825331b0f107c1dbabc3ff2
yohagos <yosef.hagos@googlemail.com>
2023-10-23 13:31:10 +0200

    project init

---
+++ src/main/java/com/midgard/MidgardApplication.java
@@ -0,0 +1,13 @@
+package com.midgard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidgardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidgardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midgard/MidgardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midgard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidgardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
