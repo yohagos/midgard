983fbb52a228288be533f97fc9203e7242e2effe
yohagos <yosef.hagos@googlemail.com>
2023-10-31 16:35:33 +0100

    created Repositories for Ticket and Comment

---
+++ src/main/java/com/midgard/comment/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.midgard.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+}
---
+++ src/main/java/com/midgard/ticket/TicketRepository.java
@@ -0,0 +1,8 @@
+package com.midgard.ticket;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
+}
