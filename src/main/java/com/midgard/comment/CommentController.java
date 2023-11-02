d9d93aaef69e3b1b0e12826b64bde63e4de14c2f
yohagos <yosef.hagos@googlemail.com>
2023-11-02 13:25:34 +0100

    added controller class for comment and ticket

---
+++ src/main/java/com/midgard/comment/CommentController.java
@@ -0,0 +1,25 @@
+package com.midgard.comment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<CommentEntity> getAllComments() {
+        return commentService.getAllComments();
+    }
+}
---
+++ src/main/java/com/midgard/ticket/TicketController.java
@@ -0,0 +1,25 @@
+package com.midgard.ticket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/ticket")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping
+    public List<TicketEntity> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+}
