d8d3c0b0998d7eda2f7c536a539f1758c11a4241
yohagos <yosef.hagos@googlemail.com>
2023-11-17 12:39:31 +0100

    feat: added TicketCategories and APIs

---
+++ src/main/java/com/midgard/ticket/TicketCategoriesResponse.java
@@ -0,0 +1,15 @@
+package com.midgard.ticket;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketCategoriesResponse {
+    private boolean update;
+    private Long ticket_id;
+}
--- src/main/java/com/midgard/util/TokenUtil.java
+++ src/main/java/com/midgard/util/TokenUtil.java
@@ -1,5 +1,7 @@
 package com.midgard.util;
 
+import java.util.List;
+
 public interface TokenUtil {
 
     String getCurrentUsername();
