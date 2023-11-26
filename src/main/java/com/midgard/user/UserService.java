028427ceb1c364b80c2657b1132680aad569141f
yohagos <yosef.hagos@googlemail.com>
2023-11-26 18:02:23 +0100

    fix: clean up II

--- src/main/java/com/midgard/user/UserService.java
+++ src/main/java/com/midgard/user/UserService.java
@@ -2,15 +2,11 @@ package com.midgard.user;
 
 
 import com.midgard.configs.JwtService;
-import com.midgard.user.requests.UserRequest;
 import com.midgard.user.responses.UserResponses;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
